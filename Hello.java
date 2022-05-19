03e26142c49bd7cb684472552bc30ce38046fa7c
Sloan Ireland <76083496+sloan-ireland@users.noreply.github.com>
2022-05-18 22:19:49 -0400

    Delete Hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-  public static void main(String[] args) {
-    System.out.print("Hello");
-    System.out.print("test1");
-    System.out.print("test0");
-    System.out.print("branch1");
-    System.out.print("branch0");
-  }
-}
